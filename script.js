74da97c2e39441d342ed102533b661671beb752e
Murillo Vinícius <Murillovinicius13@hotmail.com>
2023-03-07 20:28:28 -0300

    [UPTADE] Adicionei um som ao fim do Pomodoro

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const minute = document.querySelector('#minute')
 const second = document.querySelector('#second')
 
+
 function countdownMinute(){
   let minutes = 23
   minute.textContent = '24'
@@ -9,9 +10,10 @@ function countdownMinute(){
   minutes--
   if(minutes < 0){
     clearInterval(minuteInterval)
+    
   }
 
-  }, 60000)
+  }, 100)
  
 }
 
@@ -19,12 +21,18 @@ function countdownSecond(){
   let seconds = 58
   second.textContent = '59'
   const secondInterval = setInterval( () => {
+    
     second.textContent = seconds < 10 ? '0' + seconds : seconds
     seconds--
     if(seconds < 0){
       seconds = 59
+      if(minute.textContent === '00'){
+        const sound = new Audio('pink_panther.ogg')
+        clearInterval(secondInterval)
+        sound.play()
+      } 
     }
-  }, 1000)
+  }, 100)
 }
 
 

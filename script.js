721e98e76bdc4004cf3a3138a004c66119fc4d3c
Murillo Vinícius <Murillovinicius13@hotmail.com>
2023-03-08 16:44:04 -0300

    [UPDATE] Estilização e som do Pomodoro

--- script.js
+++ script.js
@@ -1,61 +1,59 @@
 const minute = document.querySelector('#minute')
 const second = document.querySelector('#second')
+const mensagem = document.querySelector('#mensagem')
+const timer = document.querySelector('#min25')
+let quantidadePomodoro = 0
+const sound = new Audio('Mídia/Bells.mp3')
 
 pomodoroInit()
 
-function pomodoroInit(){
-  minute.textContent = '25'
-  second.textContent = '00'
-}
-
-const timer = document.getElementById('min25')
-let quantidadePomodoro = 0
-
 timer.addEventListener('click', function(){
-  function countdownMinute(){
+  mensagem.innerHTML = ''
+  sound.pause()
+
+  function countdownMinute(){    // contagem de minutos
     let minutes = 23
     minute.textContent = '24'
     const minuteInterval = setInterval( () => {
     minute.textContent = minutes < 10 ? '0' + minutes : minutes
     minutes--
-    if(minutes < 0){
-      clearInterval(minuteInterval)
-      
-    }
-  
-    }, 10)
-   
+      if(minutes < 0){
+        clearInterval(minuteInterval)
+     } 
+    }, 60001)
   }
-
-  const mensagem = document.getElementById('mensagem')
   
-  function countdownSecond(){
+  function countdownSecond(){   // contagem de segundos
     let seconds = 58
     second.textContent = '59'
     const secondInterval = setInterval( () => {
-      
       second.textContent = seconds < 10 ? '0' + seconds : seconds
       seconds--
       if(seconds < 0){
         seconds = 59
         if(minute.textContent === '00'){
-          const sound = new Audio('pink_panther.ogg')
           clearInterval(secondInterval)
+          sound.currentTime = 0
           sound.play()
           quantidadePomodoro += 1
           if(quantidadePomodoro == 4){
-            mensagem.innerText = 'Você já utilizou o Pomodoro 4 vezes, recomendamos uma pausa de 10 minutos!!'
+            mensagem.innerHTML = '<strong style="color:#ff0446;">ATENÇÃO:</strong> Você já utilizou o Pomodoro 4 vezes, recomendamos uma pausa de 10 minutos!!'
           }
-         
         } 
       }
-    }, 10)
+    }, 1000)
   }
   
-  
+  sound.addEventListener('ended', function() {   // repetição do som ao chegar ao fim dele
+    sound.currentTime = 0 
+    sound.play()
+  })
   
   countdownMinute()
   countdownSecond()
 })
 
-
+function pomodoroInit(){
+  minute.textContent = '25'
+  second.textContent = '00'
+}

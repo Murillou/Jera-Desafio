3dbda5ed1128fe58bf3a61797308db7f58f6a4b0
Murillo Vinícius <Murillovinicius13@hotmail.com>
2023-03-08 20:27:11 -0300

    [NEW] Resetar funcionalidades

--- script.js
+++ script.js
@@ -6,100 +6,136 @@ const intervalo = document.querySelector('#intervalo')
 const audio = new Audio("Mídia/intervalEnd.mp3")
 const sound = new Audio('Mídia/Bells.mp3')
 let quantidadePomodoro = 0
+let minuteInterval
+let secondInterval
+let stopInterval
+let stopSecond
 
 pomodoroInit()
-
-pomodoro.addEventListener('click', function(){
-  mensagem.innerHTML = ''
-  audio.pause()
-  sound.pause()
-
-  function countdownMinute(){    // contagem de minutos
-    let minutes = 23
-    minute.textContent = '24'
-    const minuteInterval = setInterval( () => {
-    minute.textContent = minutes < 10 ? '0' + minutes : minutes
-    minutes--
-      if(minutes < 0){
-        clearInterval(minuteInterval)
-     } 
-    }, 60001)
-  }
-  
-  function countdownSecond(){   // contagem de segundos
-    let seconds = 58
-    second.textContent = '59'
-    const secondInterval = setInterval( () => {
-      second.textContent = seconds < 10 ? '0' + seconds : seconds
-      seconds--
-      if(seconds < 0){
-        seconds = 59
-        if(minute.textContent === '00'){
-          clearInterval(secondInterval)
-          sound.currentTime = 0
-          sound.play()
-          quantidadePomodoro += 1
-          console.log(quantidadePomodoro)
-          if(quantidadePomodoro == 4){
-            mensagem.innerHTML = '<strong style="color:#ff0446;">ATENÇÃO:</strong> Você já utilizou o Pomodoro 4 vezes, recomendamos uma pausa de 10 minutos!!'
-          }
-        } 
-      }
-    }, 1000)
-  }
-  
-  sound.addEventListener('ended', function() {   // repetição do som ao chegar ao fim dele
-    sound.currentTime = 0 
-    sound.play()
-  })
-  
-  countdownMinute()
-  countdownSecond()
-  
-})
 
 function pomodoroInit(){
   minute.textContent = '25'
   second.textContent = '00'
 }
 
+function countdownMinute(){    // contagem de minutos
+  let minutes = 23
+  minute.textContent = '24'
+  minuteInterval = setInterval( () => {
+  minute.textContent = minutes < 10 ? '0' + minutes : minutes
+  minutes--
+    if(minutes < 0){
+      clearInterval(minuteInterval)
+   } 
+  }, 100)
+}
+
+function countdownSecond(){   // contagem de segundos
+  let seconds = 58
+  second.textContent = '59'
+  secondInterval = setInterval( () => {
+    second.textContent = seconds < 10 ? '0' + seconds : seconds
+    seconds--
+    if(seconds < 0){
+      seconds = 59
+      if(minute.textContent === '00'){
+        clearInterval(secondInterval)
+        sound.currentTime = 0
+        sound.play()
+        quantidadePomodoro += 1
+        console.log(quantidadePomodoro)
+        if(quantidadePomodoro == 4){
+          mensagem.innerHTML = '<strong style="color:#ff0446;">ATENÇÃO:</strong> Você já utilizou o Pomodoro 4 vezes, recomendamos uma pausa de 10 minutos!!'
+        }
+      } 
+    }
+  }, 100)
+}
+
+
+function minutesInterval(){
+  let minutes = 3
+  minute.textContent = '04'
+   stopInterval = setInterval(() => {
+    minute.textContent = minutes < 10 ? '0' + minutes : minutes
+    minutes--
+    if(minute.textContent === '00'){
+      clearInterval(stopInterval)
+    }
+  }, 100)
+}
+
+
+function secondsInterval(){
+  let seconds = 58
+  second.textContent = '59'
+  stopSecond = setInterval(() => {
+    second.textContent = seconds < 10 ? '0' + seconds : seconds
+    seconds--
+    if(seconds < 0){
+      seconds = 59
+      if(minute.textContent == '00'){
+        clearInterval(stopSecond)
+        audio.play()
+      }
+    }
+  }, 100)
+}
+
+sound.addEventListener('ended', function() {   // repetição do som ao chegar ao fim dele
+  sound.currentTime = 0 
+  sound.play()
+})
+
+audio.addEventListener('ended', () => {
+  audio.currentTime = 0
+  audio.play()
+})
+
+function limparPomodoro(){
+  clearInterval(minuteInterval)
+  clearInterval(secondInterval)
+}
+
+function limparIntervalo(){
+  clearInterval(stopInterval)
+  clearInterval(stopSecond)
+}
+
+pomodoro.addEventListener('click', function(){
+  mensagem.innerHTML = ''
+  audio.pause()
+  sound.pause() 
+
+  limparIntervalo()
+  limparPomodoro()
+
+  countdownMinute()
+  countdownSecond()
+})
+
+
+
 intervalo.addEventListener('click', () => {
   audio.pause()
   sound.pause()
-    function minutesInterval(){
-      let minutes = 3
-      minute.textContent = '04'
-      const stopInterval = setInterval(() => {
-        minute.textContent = minutes < 10 ? '0' + minutes : minutes
-        minutes--
-        if(minute.textContent === '0'){
-          clearInterval(stopInterval)
-        }
-      }, 60001)
-    }
 
-    function secondsInterval(){
-      let seconds = 58
-      second.textContent = '59'
-      const stopSecond = setInterval(() => {
-        second.textContent = seconds < 10 ? '0' + seconds : seconds
-        seconds--
-        if(seconds < 0){
-          seconds = 59
-          if(minute.textContent == '0'){
-            clearInterval(stopSecond)
-            audio.play()
-          }
-        }
-      }, 1000)
-    }
+  limparIntervalo()
+  limparPomodoro()
 
-    audio.addEventListener('ended', () => {
-      audio.currentTime = 0
-      audio.play()
-    })
-
-    minutesInterval()
-    secondsInterval()
+  minutesInterval()
+  secondsInterval()
 })
+
+
+const reset = document.querySelector('#reset')
+
+reset.addEventListener('click', () => {
+  audio.pause()
+  sound.pause()
+  limparIntervalo()
+  limparPomodoro()
+  pomodoroInit()
+})
+
 
